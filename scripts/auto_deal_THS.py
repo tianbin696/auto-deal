a71dd8b07eb9ef245ce30859723b7c0b0c6b0fc2
TianBin <tianbin696@163.com>
2018-05-16 20:07:20 +0800

    No exit

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -269,9 +269,9 @@ class Monitor:
             try:
                 self.operation.moveMouse()
 
-                if self.compare("14", "55"):
-                    logger.info("Closed deal. Exit.")
-                    break
+                # if self.compare("14", "55"):
+                #     logger.info("Closed deal. Exit.")
+                #     break
 
                 if (self.compare("09", "32") and not self.compare("11", "28")) or (self.compare("13", "02") and not self.compare("14", "58")):
                     # 交易时间：[09:30 ~ 11:30, 13:00 ~ 15:00]
