11e305f3762e9187196dc00282af01826bfd272a
TianBin <tianbin696@163.com>
2018-05-27 15:08:50 +0800

    增加验证码识别

---
+++ scripts/yan_zhen_ma.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+import pytesseract
+from PIL import Image
+
+def get_vcode(image_path):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+    image = Image.open(image_path)
+    box = image.copy() #直接复制图像
+    box = (186, 90, 254, 110)
+    region = image.crop(box)
+    vcode = pytesseract.image_to_string(region)
+    return vcode
+
+if __name__ == "__main__":
+    vcode = get_vcode("verify_code.png")
+    print("vode: %s" % vcode)
