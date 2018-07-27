38b31d782123c2fadab35fdbf620f7882836ea1a
TianBin <tianbin696@163.com>
2018-07-27 22:41:33 +0800

    降低止盈条件，简化大盘行情判断条件，收紧买入条件

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -478,7 +478,7 @@ class Monitor:
                 # 只有当股价低于日内最高点时，才考虑卖出，避免卖出持续上涨和一字板的股票
                 # 且股价高于10日线*0.94，避免持续卖出大幅下跌的股票
 
-                if price > avg10*1.08 and price > avg1*1.02:
+                if price > avg10*1.06 and price > avg1*1.02:
                     # 股价高于10日线8%，止盈
                     return 'S'
 
@@ -494,25 +494,10 @@ class Monitor:
 
         if code not in isBuyeds or not isBuyeds[code]:
             idx_curr_price = (1+indexes['change'][0]/100)*indexes['preclose'][0] + (1+indexes['change'][12]/100)*indexes['preclose'][12] + (1+indexes['change'][17]/100)*indexes['preclose'][17]
-            idx_open_price = indexes['open'][0] + indexes['open'][12] + indexes['open'][17]
-            idx_high_price = indexes['high'][0] + indexes['high'][12] + indexes['high'][17]
             idx_low_price = indexes['low'][0] + indexes['low'][12] + indexes['low'][17]
-            if indexes['change'][0] + indexes['change'][12] + indexes['change'][17] < 0:
-                # 大盘行情不好时，不买入
+            if idx_curr_price < idx_low_price * 1.01:
                 return 'N'
-            if idx_curr_price < idx_open_price:
-                # 高开低走
-                return 'N'
-            if idx_curr_price < idx_high_price*0.99:
-                # 高处回落
-                return 'N'
-            if idx_curr_price < idx_low_price*1.004:
-                # 正在下跌
-                return 'N'
-            if price < highest_price*0.96:
-                # 避免买入高位回落股票
-                return 'N'
-            if price > avg1*1.01 and price < avg1 * 1.02 and price > open_price and price > avg10 and price < avg10*1.06 and avg10 > avg20:
+            if price > avg1*1.01 and price < avg1 * 1.02 and price > open_price and price > avg10 and price < avg10*1.04 and avg10 > avg20:
                 return 'B'
 
         return 'N'
