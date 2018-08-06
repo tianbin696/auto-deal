c8db74982a81d3c00f4362a3f2f445b58d4b12af
TianBin <tianbin696@163.com>
2018-08-06 20:52:27 +0800

    更新列表

--- scripts/stats.py
+++ scripts/stats.py
@@ -112,10 +112,10 @@ def get_code_filter_list(avg_days = 10, file = None, daysAgo = 0, timeStr=None, 
             avgs = avg(prices, avg_days)
             avg10 = numpy.mean(prices[0:avg_days])
 
-            if prices[0] <= 0 or prices[0] < avg10*0.96 or prices[0] > avg10*1.04:
+            if prices[0] <= 0 or prices[0] < avg10*0.98 or prices[0] > avg10*1.04:
                 continue
 
-            if totals[code]*prices[0] < 100:
+            if totals[code]*prices[0] < 1:
                 continue
 
             # 基于当日成交量和涨幅筛选
