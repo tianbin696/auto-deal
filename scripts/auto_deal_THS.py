7e4e20d2c525bdd12d3441c24d5129c945bd4b91
TianBin <tianbin696@163.com>
2018-08-09 20:40:13 +0800

    调整自动卖出条件，减少交易频率

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -484,10 +484,10 @@ class Monitor:
             if price > avg1*1.03 and price < highest_price*0.98:
                 # 止盈
                 return 'S'
-            if price < avg10*0.97 and price < max(avg1, highest_price)*0.97:
+            if price < avg10*0.96 and price < max(avg1, highest_price)*0.96:
                 # 趋势破位，清仓
                 return 'FS'
-            if price < max(avg1, highest_price)*0.97:
+            if price < max(avg1, highest_price)*0.96:
                 # 短期回调，止损
                 return 'S'
 
