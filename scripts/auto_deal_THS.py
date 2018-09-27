4c15ae277f8c39e8d50bbb09c0dcd81249d14c2f
tianbin <tianbin696@163.com>
2018-09-27 21:15:31 +0800

    去掉科大讯飞

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -528,7 +528,6 @@ if __name__ == '__main__':
 
             cache.clear()
             stock_codes.clear()
-            stock_codes.append("002230") # 人工智能：科大讯飞
             stock_codes.append("000538") # 大健康：云南白药
             stock_codes.append("600887") # 大消费：伊利股份
 
