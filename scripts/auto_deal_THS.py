0dbf07dbdcae962b3fb1bb7c22bfc778e3079df3
TianBin <tianbin696@163.com>
2018-11-18 08:06:18 +0800

    增加状态更新

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -358,8 +358,8 @@ class Monitor:
 
                 time.sleep(monitorInterval)
                 totalSleep += monitorInterval
-                # if totalSleep % 3600 == 0:
-                #     self.operation.saveScreenshot("状态更新", '状态更新')
+                if totalSleep % 3600 == 0:
+                    self.operation.saveScreenshot("状态更新", '状态更新')
 
                 if not isStarted:
                     continue
