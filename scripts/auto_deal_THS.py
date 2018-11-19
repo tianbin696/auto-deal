885fe88dd9295a4afa68d2b5c443c0ec1ace158d
TianBin <tianbin696@163.com>
2018-11-20 07:04:12 +0800

    fix

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -524,14 +524,14 @@ class Monitor:
         if code not in isSelleds or not isSelleds[code]:
             try:
                 self.getRealTimeMACD(code, price)
-                if price < open_price*0.96 and price > numpy.max(df['close'][1:25])*0.9 and volume > numpy.mean(df['volume'][1:6]):
+                if price < open_price*0.96 and price > numpy.max(df['close'][1:13])*0.9 and volume > numpy.mean(df['volume'][1:6]):
                     # 高位放量长阴线，卖出
                     return 'S'
             except Exception as e:
                 logger.error("Failed to calculate realtime macd of %s: %s" % (code, e))
 
         if code not in isBuyeds or not isBuyeds[code]:
-            if price > open_price*1.04 and price < numpy.min(df['close'][1:25])*1.1 and volume > numpy.mean(df['volume'][1:6]):
+            if price > open_price*1.04 and price < numpy.min(df['close'][1:13])*1.1 and volume > numpy.mean(df['volume'][1:6]):
                 # 低位放量长阳线，买入
                 return 'B'
 
