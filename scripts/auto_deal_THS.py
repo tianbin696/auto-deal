ec48c0e261a36fc070baab81f9fac8bc00a43cc7
TianBin <tianbin696@163.com>
2018-11-30 20:48:47 +0800

    enhance

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -525,14 +525,14 @@ class Monitor:
         if code not in isSelleds or not isSelleds[code]:
             try:
                 self.getRealTimeMACD(code, price)
-                if price < max(open_price, avg1)*0.96 and price > numpy.max(df['high'][1:25])*0.8 and volume > min(numpy.mean(df['volume'][1:6]), numpy.mean(df['volume'][1:11])):
+                if price < max(open_price, avg1)*0.97 and price > numpy.max(df['high'][1:25])*0.8 and volume > min(numpy.mean(df['volume'][1:6]), numpy.mean(df['volume'][1:11])):
                     # 高位放量长阴线，卖出
                     return 'S'
             except Exception as e:
                 logger.error("Failed to calculate realtime macd of %s: %s" % (code, e))
 
         if code not in isBuyeds or not isBuyeds[code]:
-            if price > min(open_price, avg1)*1.04 and price < numpy.min(df['low'][1:25])*1.2 and volume > min(numpy.mean(df['volume'][1:6]), numpy.mean(df['volume'][1:11])):
+            if price > min(open_price, avg1)*1.03 and price < numpy.min(df['low'][1:25])*1.2 and volume > min(numpy.mean(df['volume'][1:6]), numpy.mean(df['volume'][1:11])):
                 # 低位放量长阳线，买入
                 return 'B'
 
@@ -572,7 +572,7 @@ def test():
     monitor = Monitor()
 
     # Test before start
-    new_codes.append('600570')
+    new_codes.append('600621')
     for code in new_codes:
         price = monitor.getHistoryDayKAvgData(code, 1)
         real_volume = []
