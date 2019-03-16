c7c4cc7e71351511d6ed8b8939c62eccce2417be
tianbin696 <tianbin696@163.com>
2019-03-16 22:39:22 +0800

    fix

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -333,7 +333,7 @@ class Monitor:
                 self.rsis[code] = "%d-%d" % (rsi6, rsi12)
                 logger.info("RSI of %s: %d-%d" % (code, rsi6, rsi12))
 
-                macd = self.getRealTimeMACD(code, self.avg1[code])
+                macd = self.getRealTimeMACD(code, self.avg1[code][0])
                 self.macds[code] = float("%.2f" % macd[0])
                 logger.info("macd of %s: %.2f" % (code, macd[0]))
         stock_codes.clear()
@@ -344,7 +344,7 @@ class Monitor:
         end_time = time.time()
         self.operation.saveScreenshot("均值更新完成，共耗时%d秒，排除异常，可监控%d支股票。"
                                       "avg1=%s, macd=%s, rsi=%s, new_codes=%s" % ((end_time - start_time), len(stock_codes),
-                                                                       self.avg1, self.macds, self.rsis, new_codes), u'交易前准备')
+                                                                       self.avg1[0], self.macds, self.rsis, new_codes), u'交易前准备')
         logger.info("Total monitor code size: %d. Codes=%s" % (len(stock_codes), stock_codes))
 
         isStarted = False
