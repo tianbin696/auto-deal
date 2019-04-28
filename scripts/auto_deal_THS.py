dc69aecb72bf9b28ca6320c4c73b98ef28570d34
tianbin696 <tianbin696@163.com>
2019-04-29 07:26:22 +0800

    fix

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -354,9 +354,12 @@ class Monitor:
         # stock_codes.extend(sort_codes(temp_arr, avg10Days, timeStr))
         stock_codes.extend(temp_arr)
         end_time = time.time()
-        self.operation.saveScreenshot("均值更新完成，共耗时%d秒，排除异常，可监控%d支股票。"
-                                      "avg1=%s, macd=%s, rsi=%s, new_codes=%s" % ((end_time - start_time), len(stock_codes),
-                                                                       self.avg1, self.macds, self.rsis, new_codes), u'交易前准备')
+        self.operation.saveScreenshot("均值更新完成，共耗时%d秒，排除异常，可监控%d支股票。" %
+                                      ((end_time - start_time), len(stock_codes)), u'交易前准备')
+        # self.operation.saveScreenshot("均值更新完成，共耗时%d秒，排除异常，可监控%d支股票。"
+        #                               "avg1=%s, macd=%s, rsi=%s, new_codes=%s" %
+        #                               ((end_time - start_time), len(stock_codes), self.avg1, self.macds, self.rsis,
+        #                                new_codes), u'交易前准备')
         logger.info("Total monitor code size: %d. Codes=%s" % (len(stock_codes), stock_codes))
 
         isStarted = False
