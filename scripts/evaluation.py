ab8164b45fdba6f54ab1ed5aac2daf936368749a
tianbin696 <tianbin696@163.com>
2019-05-02 22:12:54 +0800

    update evaluation

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -201,7 +201,7 @@ def scan_all():
         else:
             code = "%s.SH" % code
         try:
-            test(code, startTimeStr, endTimeStr, 0.3, 0.2)
+            test(code, startTimeStr, endTimeStr, 0.3, 0.5)
         except Exception as e:
             print("%s" % e)
 
@@ -210,7 +210,7 @@ def scan_filtered():
     for code in list(open("../codes/candidates.txt")):
         code = append_loc(code.strip())
         try:
-            test(code, 20180101, 20200101, 0.3, 0.2)
+            test(code, 20190429, 20190430, 0.3, 0.5)
         except Exception as e:
             print("%s" % e)
 
