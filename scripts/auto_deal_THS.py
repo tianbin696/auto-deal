feba348be45d54f1e6b690e23eccf8befd12302e
tianbin696 <tianbin696@163.com>
2019-06-26 22:20:41 +0800

    improve from 36.57 to 38.39

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -706,6 +706,8 @@ def get_direction_by_avg(code, prices, vols, is_logging=True, open_price=0, high
             direction = 'B'
         if open_price*1.01 < prices[0] < numpy.max(prices[0:days4])*0.87 and liang_bi < 0.66:
             direction = 'B'
+        if numpy.min(prices[1:days4])*1.1 < prices[0] < numpy.max(prices[1:days4])*0.9 and liang_bi < 0.6:
+            direction = 'B'
     if diff_1 < 0 < diff_2:
         direction = 'S'
     if 0 < prices[0] < prices[1]*0.92 or 0 < prices[0] < open_price * 0.92 or 0 < prices[0] < highest_price*0.93:
