2b711891c6cb004c72436a1697d90ec6cfab875a
tianbin696 <tianbin696@163.com>
2019-08-28 22:38:17 +0800

    replace deprecated method

--- scripts/tong_hua_shun.py
+++ scripts/tong_hua_shun.py
@@ -20,7 +20,7 @@ def ths_start():
 
 def ths_restore():
     main_window = pywinauto.application.Application().connect(title =  u'网上股票交易系统5.0').top_window()
-    if main_window.HasStyle(win32defines.WS_MINIMIZE): # if minimized
+    if main_window.has_style(win32defines.WS_MINIMIZE): # if minimized
         ShowWindow(main_window.wrapper_object(), 9) # restore window state
     else:
         SetForegroundWindow(main_window.wrapper_object()) # bring to front
@@ -29,7 +29,7 @@ def ths_restore():
 def ths_close():
     ths_restore()
     time.sleep(2)
-    keyboard.SendKeys('%{F4}')
+    keyboard.send_keys('%{F4}')
 
 
 if __name__ == "__main__":
