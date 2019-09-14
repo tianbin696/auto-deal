7537515ac6976a7fb3cf6272072a1483157b2cfd
tianbin696 <tianbin696@163.com>
2019-09-14 23:59:30 +0800

    rename

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -218,24 +218,24 @@ def scan_all():
 def scan_filtered(path="../codes/candidates.txt", save_candidates=False):
     endDate = (datetime.now() - timedelta(days = 0))
     endTime = int(endDate.strftime("%Y%m%d"))
-    startTime = 20160101
+    startTime = endTime - 30000
     # st_codes = ts_local.get_st_list()
     for code in list(open(path)):
         # if code.strip() in st_codes:
         #     continue
         code = append_loc(code.strip())
         try:
-            test(code, startTime, endTime, 1.0, 5.0, save_candidates)
+            test(code, startTime, endTime, 1.0, 4.0, save_candidates)
         except Exception as e:
             print("%s" % e)
 
 
 # get_all_codes()
 # scan_all()
-# clear_candidates()
-# scan_filtered(path="../codes/all_codes.txt", save_candidates=True)
-# del all_increases[:]
-# del all_returns[:]
+clear_candidates()
+scan_filtered(path="../codes/hs_500.txt", save_candidates=True)
+del all_increases[:]
+del all_returns[:]
 scan_filtered()
 stats(all_increases)
 stats(all_returns)
