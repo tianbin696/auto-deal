010577187e672f0502dfaa98cef31580712d1545
tianbin696 <tianbin696@163.com>
2019-11-23 10:56:14 +0800

    20191123: enhance filter condition

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -226,7 +226,7 @@ def scan_filtered(path="../codes/candidates.txt", save_candidates=False):
             continue
         code = append_loc(code.strip())
         try:
-            test(code, startTime, endTime, 0.5, 4.0, save_candidates)
+            test(code, startTime, endTime, 1.0, 4.0, save_candidates)
         except Exception as e:
             print("%s" % e)
 
