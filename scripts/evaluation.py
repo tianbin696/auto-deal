9bd2e4e9186e3a1ab94f1a22673e9bb65bc94c7b
tianbin696 <tianbin696@163.com>
2019-11-24 15:14:53 +0800

    20191124: update evaluation filter

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -124,11 +124,9 @@ def test(code, start_date=20100101, end_date=20200101, expect_return=0.8, expect
             print("NaN of code: %s" % code)
             exit(1)
         if stock.returns[last_index] > max(0.50, stock.increases[last_index]*1.5) \
-                and numpy.max(stock.increases) < numpy.min(stock.increases)*1.5 \
                 and stock.increases[last_index] < 1.5 \
-                and numpy.mean(stock.returns) > numpy.mean(stock.increases)*1.5 \
-                and stock.returns[last_index] > numpy.max(stock.returns)*0.70 \
-                and stock.increases[last_index] > numpy.max(stock.increases)*0.70:
+                and numpy.mean(stock.returns) > numpy.mean(stock.increases)*2.0 \
+                and stock.returns[last_index] > numpy.max(stock.returns)*0.70:
             if save_to_candidates:
                 save_2_candidates(code)
             stock.print_as_csv("../analyze/%s_%s.csv" % (code, stock.start_date))
