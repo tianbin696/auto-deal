4b3bb3900ff0423091d795e59e4ca5b07385b6d1
tianbin696 <tianbin696@163.com>
2020-03-08 09:00:46 +0800

    20050520-20200306: Avg:13.52, Sum:37540.67, Days:98.08

--- scripts/auto_deal_THS.py
+++ scripts/auto_deal_THS.py
@@ -771,9 +771,9 @@ def get_direction_for_lianban(code, prices, vols, is_logging=True, open_price=0,
     avg_2 = numpy.mean(prices[0:10])
     if avg_1 > avg_2 and max(max(prices[1], open_price)*1.01, highest_price*0.98) < prices[0] < prices[1]*1.05:
         return 'B'
-    if avg_1 < avg_2 and prices[0] < min(open_price, prices[1])*0.93:
+    if avg_1 < avg_2 and prices[0] < min(open_price, prices[1])*0.94:
         return 'S'
-    if prices[0] < highest_price*0.92:
+    if prices[0] < highest_price*0.92 or prices[0] < min(open_price, prices[1])*0.93:
         return 'S'
     return 'B'
 
