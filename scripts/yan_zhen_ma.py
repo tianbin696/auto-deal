1b4fc590363f231c9bcec2b40fe959779e50aee9
tianbin696 <tianbin696@163.com>
2021-07-31 07:00:38 +0800

    format

--- scripts/yan_zhen_ma.py
+++ scripts/yan_zhen_ma.py
@@ -3,6 +3,7 @@
 import pytesseract
 from PIL import Image
 
+
 def get_vcode(image_path):
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
     image = Image.open(image_path)
@@ -11,6 +12,7 @@ def get_vcode(image_path):
     region = image.crop(box)
     vcode = pytesseract.image_to_string(region)
     return vcode
+
 
 if __name__ == "__main__":
     vcode = get_vcode("verify_code.png")
