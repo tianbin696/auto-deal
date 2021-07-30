9903bb4888b5b594341d2ab1aa58c4be028ca8e9
tianbin696 <tianbin696@163.com>
2021-07-30 23:04:38 +0800

    bs_direction_macd: initial add

---
+++ scripts/bs_direction_macd.py
@@ -0,0 +1,45 @@
+#/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import ts_cli as ts
+
+
+def get_direction(df):
+    __df = __get_macd(df, 12, 26, 9)
+    if __df['macd'][0] > 0 > __df['macd'][1]:
+        return 'B'
+    if __df['macd'][0] < 0 < __df['macd'][1]:
+        return 'S'
+    return 'N'
+
+
+def __get_ema(df, N):
+    for i in range(len(df)-1, -1, -1):
+        if i == len(df)-1:
+            df.ix[i, 'ema'] = df.ix[i, 'close']
+        if i < len(df)-1:
+            df.ix[i, 'ema'] = (2*df.ix[i, 'close']+(N-1)*df.ix[i+1, 'ema'])/(N+1)
+    __ema = list(df['ema'])
+    return __ema
+
+
+def __get_macd(df, short=12, long=26, M=9):
+    a = __get_ema(df, short)
+    b = __get_ema(df, long)
+    df['diff'] = pd.Series(a)-pd.Series(b)
+    for i in range(len(df)-1, -1, -1):
+        if i == len(df)-1:
+            df.ix[i, 'dea'] = df.ix[i, 'diff']
+        if i < len(df)-1:
+            df.ix[i, 'dea'] = (2*df.ix[i, 'diff']+(M-1)*df.ix[i+1, 'dea'])/(M+1)
+    df['macd'] = 2*(df['diff']-df['dea'])
+    return df
+
+
+if __name__ == "__main__":
+    df = ts.get_h_data("600570.SH", start_date="20210701", end_date="20210730")
+    df = __get_macd(df, 12, 26, 9)
+    print df[0:30]
+
+    direction = get_direction(df)
+    print direction
