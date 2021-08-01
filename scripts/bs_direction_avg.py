6c1d0f70582568b46024925a7eed0b22a5895316
tianbin696 <tianbin696@163.com>
2021-08-01 09:04:12 +0800

    improve deal based on average

--- scripts/bs_direction_avg.py
+++ scripts/bs_direction_avg.py
@@ -7,24 +7,21 @@ import numpy
 def get_direction(prices):
     avg_01 = numpy.mean(prices[0:5])
     avg_02 = numpy.mean(prices[0:10])
-    avg_03 = numpy.mean(prices[0:30])
 
     avg_11 = numpy.mean(prices[1:6])
     avg_12 = numpy.mean(prices[1:11])
-    avg_13 = numpy.mean(prices[1:31])
 
-    if avg_12 > avg_11 > avg_13:
-        if avg_01 > avg_02 > avg_03:
+    if avg_12 > avg_11:
+        if avg_01 > avg_02:
             return "B"
-    if avg_11 > avg_12 > avg_13:
-        if avg_02 > avg_01 > avg_03:
+    if avg_11 > avg_12:
+        if avg_02 > avg_01:
             return "S"
     return "N"
 
 
 if __name__ == "__main__":
-    df = ts.get_h_data("600570.SH", start_date="20200730", end_date="20210730")
-
+    df = ts.get_h_data("601100.SH", start_date="20200730", end_date="20210730")
     for i in range(0, 120):
         updated_prices = []
         updated_prices.extend(df['close'][i:])
