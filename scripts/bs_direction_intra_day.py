527ddae1525c0bfc298e913ec1283163ab943513
tianbin696 <tianbin696@163.com>
2021-08-01 15:28:20 +0800

    improve intra day deal

--- scripts/bs_direction_intra_day.py
+++ scripts/bs_direction_intra_day.py
@@ -6,18 +6,19 @@ import ts_cli as ts
 
 def get_direction(price_in, df_h_in, days_in=10):
     upper_line = numpy.max(df_h_in['high'][0:days_in])
-    if price_in > upper_line:
-        return "B"
+    if numpy.mean(df_h_in['close'][0:5]) < price_in < numpy.mean(df_h_in['close'][0:5])*1.20:
+        if price_in > upper_line:
+            return "B"
     return "N"
 
 
 if __name__ == "__main__":
     for code in ['002032.SZ', '002311.SZ', '002271.SZ', '600161.SH', '601100.SH', '603899.SH']:
-        df = ts.get_h_data(code, start_date="20200730", end_date="20210730")
+        df = ts.get_h_data(code, start_date="20150730", end_date="20210730")
         count = 0
         total_profit = 0
         days = 10
-        for i in range(1, 120):
+        for i in range(1, 300):
             __df = df[i+1:].reset_index()
             direction = get_direction(df['high'][i], __df, days)
             if direction == "B":
