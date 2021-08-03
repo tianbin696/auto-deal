d30eb60b45eb3ee6f0545513aaa2e0384666be11
tianbin696 <tianbin696@163.com>
2021-08-03 18:14:41 +0800

    update test case

--- scripts/ts_cli.py
+++ scripts/ts_cli.py
@@ -1,12 +1,13 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+import datetime
 import tushare as ts
 
 token = "3f249c0d4e9d93e62dd5a5b2c96dd75272e715fbfe8e972bf786aa9f"
 ts.set_token(token)
 
 
-def get_h_data(code='600570.SH', start_date='20200101', end_date='20212001'):
+def get_h_data(code='600570.SH', start_date='20200101', end_date='20210101'):
     __df = ts.pro_bar(ts_code=code, adj="qfq", start_date=start_date, end_date=end_date)
     return __df
 
@@ -20,7 +21,8 @@ if __name__ == "__main__":
     df = get_h_data("600570.SH")
     print df[0:3]
 
-    df = get_h_data("600570.SH", end_date="20210801")
+    end_date_str = (datetime.datetime.now()).strftime("%Y%m%d")
+    df = get_h_data("600570.SH", end_date=end_date_str)
     print df[0:3]
 
     df = get_rt_price("600570")
