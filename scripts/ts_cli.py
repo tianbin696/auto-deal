5227ea9bf11cbe1ca594fc20b92a6f894d4e97b8
tianbin696 <tianbin696@163.com>
2021-08-06 06:22:47 +0800

    add cache for historical data

--- scripts/ts_cli.py
+++ scripts/ts_cli.py
@@ -1,5 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import pandas
 import datetime
 import tushare as ts
 
@@ -17,6 +19,36 @@ def get_rt_price(code):
     return __df
 
 
+def get_h_data_cache(code='600570.SH', start_date='20200101', end_date='20210101', force_update=False):
+    cache_folder = "../../cache"
+    cache_file = cache_folder + "/" + code + ".csv"
+    if not os.path.exists(cache_folder):
+        os.mkdir(cache_folder)
+
+    if os.path.exists(cache_file) and not force_update:
+        __df = pandas.read_csv(cache_file)
+    else:
+        __df = ts.pro_bar(ts_code=code, adj="qfq", start_date=start_date, end_date=end_date)
+        if len(__df) > 0:
+            writer = open(cache_file, "w")
+            __df.to_csv(writer)
+            writer.close()
+        __df = pandas.read_csv(cache_file)
+    return __df
+
+
+def update_cache():
+    for __code in list(open("code_hs_300.txt")):
+        __code = __code.strip()
+        if __code.startswith('0') or __code.startswith('3'):
+            __code = "%s.SZ" % __code
+        else:
+            __code = "%s.SH" % __code
+        end_date = (datetime.datetime.now()).strftime("%Y%m%d")
+        get_h_data_cache(__code, "20100101", end_date, True)
+        print "cached %s" % __code
+
+
 if __name__ == "__main__":
     df = get_h_data("600570.SH")
     print df[0:3]
@@ -27,3 +59,5 @@ if __name__ == "__main__":
 
     df = get_rt_price("600570")
     print df
+
+    update_cache()
